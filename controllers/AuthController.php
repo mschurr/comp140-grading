e160ab1cd62d6d2837a590e2adaacded173803ed
Matthew Schurr <mschurr@rice.edu>
2014-09-24 20:22:17 -0500

    CAS to persistent logins.

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -12,9 +12,9 @@ class AuthController extends Controller
 			$this->session['_login_redirect'] = $this->request->get['url'];
 		else
 			$this->session['_login_redirect'] = '/';
-		
+
 		try {
-			$result = $this->auth->attempt(null, null);
+			$result = $this->auth->attempt(null, null, true);
 			$url = isset($this->session['_login_redirect']) ? URL::to($this->session['_login_redirect']) : URL::to('/');
 			unset($this->session['_login_redirect']);
 			if($url === null || !$url->isInternal()) $url = URL::to('/');
@@ -33,11 +33,11 @@ class AuthController extends Controller
 		// Determine destination.
 		$destination = isset($this->request->get['url']) ? URL::to($this->request->get['url']) : URL::to('/');
 		if($destination === null || !$destination->isInternal()) $destination = URL::to('/');
-		
+
 		// If the user is already logged in, redirect.
 		if($this->auth->loggedIn())
 			return Redirect::to($destination);
-		
+
 		// Otherwise, let's display the form.
 		return View::make('BookExchange.Login')->with(array(
 			'user' => $this->user,
@@ -46,10 +46,10 @@ class AuthController extends Controller
 			'destination' => isset($this->request->post['destination']) ? $this->request->post['destination'] : $destination
 		));
 	}
-	
+
 	public function loginAction()
-	{	
-		// Determine the redirection URL.		
+	{
+		// Determine the redirection URL.
 		if(isset($this->request->post['destination'])) {
 			$url = URL::to($this->request->post['destination']);
 			if($url === null || !$url->isInternal()) $url = URL::to('/');
@@ -57,28 +57,28 @@ class AuthController extends Controller
 		else {
 			$url = URL::to('/');
 		}
-		
+
 		// If the user is already logged in, just redirect.
 		if($this->auth->loggedIn())
 			return Redirect::to($url);
-		
+
 		// Protecte against CSRF attacks.
 		//if(!CSRF::check('login', $this->request->post['_csrf']))
 		//	return $this->get("The server detected a problem with your request; try again.");
-		
+
 		// Ensure that the user sent us the mandatory information.
-		if(!isset($this->request->post['username']) 
-		|| !isset($this->request->post['password']) 
-		|| strlen($this->request->post['username']) == 0 
+		if(!isset($this->request->post['username'])
+		|| !isset($this->request->post['password'])
+		|| strlen($this->request->post['username']) == 0
 		|| strlen($this->request->post['password']) == 0
 		|| strlen($this->request->post['username']) > 50
 		|| strlen($this->request->post['password']) > 50)
 			return $this->login("You must enter a username and password.");
-		
+
 		try {
 			// Attempt the login.
 			$this->auth->attempt($this->request->post['username'], $this->request->post['password'], isset($this->request->post['persistent']));
-			
+
 			// Redirect on success.
 			return Redirect::to($url);
 		}
@@ -100,13 +100,13 @@ class AuthController extends Controller
 		if($this->auth->loggedIn) {
 			$this->auth->logout();
 		}
-		
+
 		// Redirect the user to _GET[url] if set or / otherwise.
 		$url = isset($this->session['_logout_redirect']) ? URL::to($this->session['_logout_redirect']) : URL::to('/');
 		unset($this->session['_logout_redirect']);
 
 		if($url === null || !$url->isInternal()) $url = URL::to('/');
-		
+
 		return Redirect::to($url);
 	}
 }
